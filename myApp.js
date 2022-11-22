9d473f06ab1c87eabfa3700492746fd2525665b9
Matt Roby <matt.r331@gmail.com>
2022-11-21 22:18:47 -0700

    FCC lessons

--- myApp.js
+++ myApp.js
@@ -1,39 +1,20 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config();
 
+console.log("Hello World");
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+    var myMessage = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase")
+        myMessage = myMessage.toUpperCase();
+    res.json({ message: myMessage });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
